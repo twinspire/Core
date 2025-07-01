f2cd03d09f87a5dc65a7c986ed1726e6685fee96
tienery <luke@lukeselman.com>
2025-07-01 11:09:04 +0100

    Add None property to Id.

--- twinspire/Id.hx
+++ twinspire/Id.hx
@@ -9,4 +9,16 @@ abstract Id(Int) {
         this = _lastId;
     }
 
+    static var _none:Id;
+    /**
+    * Get an Id value that is considered not to reference anything.
+    **/
+    public static var None(get, never):Id;
+    static function get_None() {
+        if (_none == null) {
+            _none = new Id();
+        }
+        return _none;
+    }
+
 }
